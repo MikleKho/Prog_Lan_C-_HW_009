af818b12dbc587ad35c4723bbfa7b70427877f49
«MikleKho» <khomich.mikhail@gmail.com>
2022-09-07 19:07:27 +0300

    Решение Task_02

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -2,3 +2,30 @@
 // натуральных элементов в промежутке от M до N с помощью рекурсии.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8 -> 30
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SummarizeNumbers(int m, int n, int sum)
+{
+    if (m <= n)
+    {
+        sum = sum + m;
+    }
+
+    if (m == n) return (sum);
+    m++;
+    return (SummarizeNumbers(m, n, sum));
+}
+
+int m = Prompt("Введите меньшее натуральное число ---> ");
+int n = Prompt("Введите большее натуральное число ---> ");
+int sum = 0;
+if (m < n)
+{
+    sum = SummarizeNumbers(m, n, sum);
+    System.Console.WriteLine($"Сумма чисел ---> {sum}");
+}
+else System.Console.WriteLine("Ошибка ввода");
